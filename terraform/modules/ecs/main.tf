496822b8c7eba43540caff2f19382c0398aa4f0b
Yassin Suleiman <yassine.suleiman@proton.me>
2026-01-27 00:12:11 +0100

    add: deployment percentages

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -67,6 +67,8 @@ resource "aws_ecs_service" "main" {
   desired_count   = var.app_count
   launch_type     = "FARGATE"
   
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 20
  lifecycle {
     ignore_changes = [task_definition]
   }
